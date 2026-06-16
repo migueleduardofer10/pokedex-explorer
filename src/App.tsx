877d0d8244bbf6bd62a004518db76df6500fedb1
Miguel Fernández <miguel.fernandez@guinea.pe>
2026-06-16 18:33:50 -0500

    style: add view transitions and card animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import ListPage from './pages/ListPage'
 import DetailPage from './pages/DetailPage'
@@ -6,16 +6,21 @@ import FavoritesPage from './pages/FavoritesPage'
 import './App.css'
 
 function App() {
+  const location = useLocation()
+
   return (
     <>
       <Navbar />
       <main className="container">
-        <Routes>
-          <Route path="/" element={<ListPage />} />
-          <Route path="/pokemon/:id" element={<DetailPage />} />
-          <Route path="/favoritos" element={<FavoritesPage />} />
-          <Route path="*" element={<p>Página no encontrada (404)</p>} />
-        </Routes>
+        {/* key por ruta: reinicia la animación de fade en cada navegación */}
+        <div className="page" key={location.pathname}>
+          <Routes location={location}>
+            <Route path="/" element={<ListPage />} />
+            <Route path="/pokemon/:id" element={<DetailPage />} />
+            <Route path="/favoritos" element={<FavoritesPage />} />
+            <Route path="*" element={<p>Página no encontrada (404)</p>} />
+          </Routes>
+        </div>
       </main>
     </>
   )
